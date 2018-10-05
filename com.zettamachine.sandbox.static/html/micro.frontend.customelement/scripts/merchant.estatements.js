9339772afec90ab1d55d13173507a677998ecdf8
Makiato von Zetta <hello@zettamachine.com>
2018-10-05 13:10:30 +1000

    regular

--- com.zettamachine.sandbox.static/html/micro.frontend.customelement/scripts/merchant.estatements.js
+++ com.zettamachine.sandbox.static/html/micro.frontend.customelement/scripts/merchant.estatements.js
@@ -5,8 +5,6 @@ const messages = {
 class MerchantEStatements extends HTMLElement {
 
 	constructor() {
-		// If you define a constructor, always call super() first!
-		// This is specific to CE and required by the spec.
 		super();
 	}
 	
@@ -15,11 +13,11 @@ class MerchantEStatements extends HTMLElement {
 			<div style="border:1px solid #ddd; width: 500px; height: 100px;padding: 10px">
 				<form>
 					Select Site: <select name="site" id="site">
-					<option name="barangaroo">Barangaroo</option>
-					<option name="kent">Kent</option>
+						<option name="barangaroo">Barangaroo</option>
+						<option name="kent">Kent</option>
 					</select>
 				</form>
-				<button onclick="alert(${messages.hello});">${messages.hello}</button>
+				<button onclick="alert('${messages.hello}');">${messages.hello}</button>
 			</div>`;
 	}
 
