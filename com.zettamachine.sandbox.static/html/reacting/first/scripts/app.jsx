31186ded7a34c82a351adf8b534cc5a6981809e0
Makiato von Zetta <hello@zettamachine.com>
2018-10-09 17:20:56 +1100

    reacting

--- com.zettamachine.sandbox.static/html/reacting/first/scripts/app.jsx
+++ com.zettamachine.sandbox.static/html/reacting/first/scripts/app.jsx
@@ -3,22 +3,39 @@ class Hello extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			user: "Alfred"	
+			user: 'Alfred',
+			color: 'red',
+			articles: [] 	
 		};
 		this.update = this.update.bind(this); 
+		this.componentDidMount = this.componentDidMount.bind(this);
+	}
+	
+	componentDidMount() {
+		//axios.get('http://jsonplaceholder.typicode.com/todos')
+		axios.get('data/articles.json')
+		.then(function (response) {
+			console.log(response.data.data.articles);
+			//this.setState({color: 'green'});
+		})
+		.catch(function (error) {
+		   	console.log('Error caught -> ' + error);
+		});   
 	}
 	
 	render() {
+		var articleList = this.state.articles.map((article) => <li>{article.title}</li>);
 		return (
 			<div>
-				<h1 style={{color:red}}>Hello {this.state.user}</h1>
+				<h1 style={{color: `${this.state.color}`}}>Hello {this.state.user}</h1>
 				<button onClick={this.update}>click</button>
+				<ul>{articleList}</ul>
 			</div>
 		)
 	}
 	
 	update() {
-		this.setState({user: "Joker"});
+		this.setState({user: 'Joker', color: 'blue'});
 	}
 }
 
