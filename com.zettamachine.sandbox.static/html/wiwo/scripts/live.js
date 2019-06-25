3f5faa92bea240a62b7b0b19ece0d2798f6fdd7d
AYZMTQ <AYZMTQ@AI850310.au.amp.local>
2019-06-25 15:22:06 +1000

    added more to wiwo

---
+++ com.zettamachine.sandbox.static/html/wiwo/scripts/live.js
@@ -0,0 +1,9 @@
+(function(){
+    var wiwo = window.wiwo || (window.wiwo = {});
+    //wiwo.license_config = {"license_key":"bimade","product":"wiwo-repayment-widget","product_rev":-1,"env":"live","widget_url":"https://d2q15xs2m2nrip.cloudfront.net/wiwo/wiwo-repayment-widget/18.1.0/clients/wiwo/index.html","version":39,"client_resources":[],"config_url":"https://calcs.widgetworks.com.au/c/bimade/live/39"};
+    
+    wiwo.license_config = {"license_key":"bimade","product":"wiwo-repayment-widget","product_rev":-1,"env":"live","widget_url":"https://d2q15xs2m2nrip.cloudfront.net/wiwo/wiwo-repayment-widget/18.1.0/clients/wiwo/index.html","version":39,"client_resources":[],"config_url":"scripts/wiwo.json"};
+    
+    wiwo.license_configs = wiwo.license_configs || (wiwo.license_configs = []);
+    wiwo.license_configs.push(wiwo.license_config);
+}(window));
