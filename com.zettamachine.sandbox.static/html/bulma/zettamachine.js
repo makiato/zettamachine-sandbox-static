bdfa229c81cda5ac1934170274ca71d15b0f19a9
Mehmet <mehmet@zettamachine.com>
2019-07-08 09:52:11 +1000

    updated js

--- com.zettamachine.sandbox.static/html/bulma/zettamachine.js
+++ com.zettamachine.sandbox.static/html/bulma/zettamachine.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
         var target = $el.dataset.target;
         var $target = document.getElementById(target);
         $el.classList.toggle('is-active');
-        $el.classList.toggle('has-dark-background');
         $target.classList.toggle('is-active');
         $target.classList.toggle('has-dark-background');
       });
